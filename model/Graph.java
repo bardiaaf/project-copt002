34ec3a5ebf3e8f104092a23a21f25491d49116c2
bardia <bardia.aryanfard@gmail.com>
2022-07-15 20:06:57 +0430

    Bardiya: models 2022/7/15

--- model/Graph.java
+++ model/Graph.java
@@ -1,16 +1,26 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Graph {
     public final Vertex[] vertices;
     public final Edge[][] a;
+    public final List<Edge>[] adj;
 
     public Graph(Edge[][] a) {
         vertices = new Vertex[a.length];
         for (int i = 0; i < a.length; i++)
             vertices[i] = new Vertex(i);
         this.a = a;
+        this.adj = new List[vertices.length];
+        for (Vertex v: vertices) {
+            adj[v.id] = new ArrayList<>();
+            for(Vertex u: vertices)
+                if(hasEdge(v,u))
+                    adj[v.id].add(a[v.id][u.id]);
+            adj[v.id].sort(Edge::compareTo);
+        }
     }
 
     public Edge getEdge(Vertex v, Vertex u) {

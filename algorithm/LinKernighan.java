8be8b091f372b22d984c9e1600c270a94d992335
heliaYazdanyar <helia79yaz@gmail.com>
2022-07-17 22:46:05 +0430

    locality of best KExchange

--- algorithm/LinKernighan.java
+++ algorithm/LinKernighan.java
@@ -2,6 +2,7 @@ package algorithm;
 
 import model.*;
 
+import java.awt.event.KeyEvent;
 import java.util.List;
 
 public class LinKernighan {
@@ -10,27 +11,40 @@ public class LinKernighan {
     public static K_Exchange bestKExchange;
 
     // "l" is number of neighbors
-    public static void linKernighan(Tour T, int k, int l){
+    public static void linKernighanIteration(Tour T, int k, int l){
+        int bestGain=0;
+        K_Exchange best = null;
         // for each starting vertex v
-        for (int i=0;i<Graph.graph.getSize();i++)
-            linKernighanFirst(T, k , l, Graph.graph.getVertex(i));
+        for (int i=0;i<Graph.graph.getSize();i++) {
+            K_Exchange stepResult =linKernighanFirst(T, k, l, Graph.graph.getVertex(i));
+            if (stepResult.getGain()>bestGain){
+                best = stepResult;
+                bestGain = best.getGain();
+            }
+        }
 
     }
 
-    public static void linKernighanFirst(Tour T, int k, int l, Vertex first){
-        linKernighanSecond(T, k, l, first, T.next(first));
-        linKernighanSecond(T, k, l, first, T.previous(first));
+    public static K_Exchange  linKernighanFirst(Tour T, int k, int l, Vertex first){
+        K_Exchange k1 = linKernighanSecond(T, k, l, first, T.next(first));
+        K_Exchange k2 = linKernighanSecond(T, k, l, first, T.previous(first));
+
+        if (k1.getGain() > k2.getGain())
+            return k1;
+
+        return k2;
     }
 
-    public static void linKernighanSecond(Tour T, int k, int l, Vertex first, Vertex second){
+    public static K_Exchange  linKernighanSecond(Tour T, int k, int l, Vertex first, Vertex second){
         K_Exchange kExchange = new K_Exchange(Graph.graph, T,first, second);
-        Vertex currentVertex = second;
 
-        applyStep(kExchange, 0, k , l, currentVertex);
+        return applyStep(kExchange, 0, k , l, second);
     }
 
-    private static void applyStep(K_Exchange kExchange, int step, int k, int l, Vertex currentVertex){
+    private static K_Exchange applyStep(K_Exchange kExchange, int step, int k, int l, Vertex currentVertex){
         List<Edge> list = Graph.graph.nearestNeighbors(currentVertex, l);
+
+        K_Exchange best = kExchange;
 
         for (Edge e:
              list) {
@@ -39,29 +53,24 @@ public class LinKernighan {
                 K_Exchange newKExchange = kExchange.add(e);
 
                 if(newKExchange!=null){
+                    // che vaghti ke apply step mikoni va che vaghti ke miri toye else max bayad begirim ba ye best ii ke aval darim
                     if (step+1<k) {
                         step++; // for each read and blue together step++
-                        applyStep(newKExchange, step, k, l, newKExchange.t.get(newKExchange.t.size()-1));
+                        best = applyStep(newKExchange, step, k, l, newKExchange.t.get(newKExchange.t.size()-1));
                     }
                     else{ // reached step k
 
-                        if(bestKExchange==null)
-                            bestKExchange = newKExchange;
-                        else if(newKExchange.getGain() > bestKExchange.getGain())
-                            bestKExchange = newKExchange;
-
-                        newKExchange.close();
+                        if(newKExchange.getGain() > best.getGain()){
+                            best = newKExchange;
+                        }
                     }
                 }
             }
         }
 
-        if(bestKExchange==null)
-            bestKExchange = kExchange;
-        else if(kExchange.getGain() > bestKExchange.getGain())
-            bestKExchange = kExchange;
 
-        kExchange.close();
+        best.close();
+        return best;
 
     }
 
